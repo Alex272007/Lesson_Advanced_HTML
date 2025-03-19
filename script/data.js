7098baa23ae3bf15c1bf3062dc0c940abae01616
Alex272007 <154067464+Alex272007@users.noreply.github.com>
2025-03-19 20:37:47 +0400

    add file

---
+++ script/data.js
@@ -0,0 +1,18 @@
+const dataProducts = `
+    [
+        {
+            "url": "/img/image3.webp",
+            "name": "MANGO  PEOPLE  T-SHIRT",
+            "price": "300",
+            "color": "Red",
+            "size": "XL"   
+        },
+        {
+            "url": "/img/image8.webp",
+            "name": "ELLERY X M'O CAPSULE",
+            "price": "52",
+            "color": "Black",
+            "size": "XL"
+        }
+    ]
+`
